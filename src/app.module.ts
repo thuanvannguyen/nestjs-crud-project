b6afed5f68b24ee29f8d1e03034f674cd40eded6
thuan.nv <nguyenthuankma@gmail.com>
2024-07-01 16:52:56 +0700

    Complete CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateTodoDto } from './dtos/create-todo.dto';
+import { TodosService } from './todos.service';
+
+@Controller('todos')
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Post()
+  create(@Body() dto: CreateTodoDto) {
+    return this.todosService.create(dto);
+  }
+
+  @Get()
+  findMany() {
+    return this.todosService.findMany();
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() dto: CreateTodoDto) {
+    return this.todosService.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number, @Body() dto: CreateTodoDto) {
+    return this.todosService.delete(id);
+  }
+}
